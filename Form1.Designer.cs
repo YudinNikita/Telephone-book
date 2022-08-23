4b3ab573f8785bc3cfe8ce5d0bab48203db27353
YudinNikita <yudin.nikita.pers@gmail3.com>
2022-08-23 13:09:20 +0300

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace Telephone_Book
+{
+    partial class TelephoneBook
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.newButton = new System.Windows.Forms.Button();
+            this.loadButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.firstNameLabel = new System.Windows.Forms.Label();
+            this.lastNameLabel = new System.Windows.Forms.Label();
+            this.phoneNumberLabel = new System.Windows.Forms.Label();
+            this.emailLabel = new System.Windows.Forms.Label();
+            this.firstNameTextBox = new System.Windows.Forms.TextBox();
+            this.lastnameTextBox = new System.Windows.Forms.TextBox();
+            this.phoneNumberTextBox = new System.Windows.Forms.TextBox();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // newButton
+            // 
+            this.newButton.Location = new System.Drawing.Point(699, 36);
+            this.newButton.Name = "newButton";
+            this.newButton.Padding = new System.Windows.Forms.Padding(3);
+            this.newButton.Size = new System.Drawing.Size(75, 25);
+            this.newButton.TabIndex = 0;
+            this.newButton.Text = "New";
+            this.newButton.UseVisualStyleBackColor = true;
+            this.newButton.Click += new System.EventHandler(this.newButton_Click);
+            // 
+            // loadButton
+            // 
+            this.loadButton.Location = new System.Drawing.Point(699, 67);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Padding = new System.Windows.Forms.Padding(3);
+            this.loadButton.Size = new System.Drawing.Size(75, 25);
+            this.loadButton.TabIndex = 1;
+            this.loadButton.Text = "Load";
+            this.loadButton.UseVisualStyleBackColor = true;
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(699, 98);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Padding = new System.Windows.Forms.Padding(3);
+            this.saveButton.Size = new System.Drawing.Size(75, 25);
+            this.saveButton.TabIndex = 2;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(699, 129);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Padding = new System.Windows.Forms.Padding(3);
+            this.deleteButton.Size = new System.Drawing.Size(75, 25);
+            this.deleteButton.TabIndex = 3;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // firstNameLabel
+            // 
+            this.firstNameLabel.AutoSize = true;
+            this.firstNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.firstNameLabel.Location = new System.Drawing.Point(12, 36);
+            this.firstNameLabel.Name = "firstNameLabel";
+            this.firstNameLabel.Size = new System.Drawing.Size(112, 25);
+            this.firstNameLabel.TabIndex = 4;
+            this.firstNameLabel.Text = "First Name:";
+            // 
+            // lastNameLabel
+            // 
+            this.lastNameLabel.AutoSize = true;
+            this.lastNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.lastNameLabel.Location = new System.Drawing.Point(12, 67);
+            this.lastNameLabel.Name = "lastNameLabel";
+            this.lastNameLabel.Size = new System.Drawing.Size(112, 25);
+            this.lastNameLabel.TabIndex = 5;
+            this.lastNameLabel.Text = "Last Name:";
+            // 
+            // phoneNumberLabel
+            // 
+            this.phoneNumberLabel.AutoSize = true;
+            this.phoneNumberLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.phoneNumberLabel.Location = new System.Drawing.Point(12, 98);
+            this.phoneNumberLabel.Name = "phoneNumberLabel";
+            this.phoneNumberLabel.Size = new System.Drawing.Size(146, 25);
+            this.phoneNumberLabel.TabIndex = 6;
+            this.phoneNumberLabel.Text = "Phone number:";
+            // 
+            // emailLabel
+            // 
+            this.emailLabel.AutoSize = true;
+            this.emailLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.emailLabel.Location = new System.Drawing.Point(12, 129);
+            this.emailLabel.Name = "emailLabel";
+            this.emailLabel.Size = new System.Drawing.Size(71, 25);
+            this.emailLabel.TabIndex = 7;
+            this.emailLabel.Text = "Email: ";
+            // 
+            // firstNameTextBox
+            // 
+            this.firstNameTextBox.Location = new System.Drawing.Point(159, 36);
+            this.firstNameTextBox.Multiline = true;
+            this.firstNameTextBox.Name = "firstNameTextBox";
+            this.firstNameTextBox.Size = new System.Drawing.Size(534, 25);
+            this.firstNameTextBox.TabIndex = 8;
+            // 
+            // lastnameTextBox
+            // 
+            this.lastnameTextBox.Location = new System.Drawing.Point(159, 67);
+            this.lastnameTextBox.Multiline = true;
+            this.lastnameTextBox.Name = "lastnameTextBox";
+            this.lastnameTextBox.Size = new System.Drawing.Size(534, 25);
+            this.lastnameTextBox.TabIndex = 9;
+            // 
+            // phoneNumberTextBox
+            // 
+            this.phoneNumberTextBox.Location = new System.Drawing.Point(159, 98);
+            this.phoneNumberTextBox.Multiline = true;
+            this.phoneNumberTextBox.Name = "phoneNumberTextBox";
+            this.phoneNumberTextBox.Size = new System.Drawing.Size(534, 25);
+            this.phoneNumberTextBox.TabIndex = 10;
+            this.phoneNumberTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.phoneNumberTextBox_KeyPress);
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.Location = new System.Drawing.Point(159, 126);
+            this.emailTextBox.Multiline = true;
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(534, 25);
+            this.emailTextBox.TabIndex = 11;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(17, 196);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(757, 200);
+            this.dataGridView1.TabIndex = 12;
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            // 
+            // TelephoneBook
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.emailTextBox);
+            this.Controls.Add(this.phoneNumberTextBox);
+            this.Controls.Add(this.lastnameTextBox);
+            this.Controls.Add(this.firstNameTextBox);
+            this.Controls.Add(this.emailLabel);
+            this.Controls.Add(this.phoneNumberLabel);
+            this.Controls.Add(this.lastNameLabel);
+            this.Controls.Add(this.firstNameLabel);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.loadButton);
+            this.Controls.Add(this.newButton);
+            this.Name = "TelephoneBook";
+            this.Text = "Telephone Book";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.TelephoneBook_FormClosing);
+            this.Load += new System.EventHandler(this.TelephoneBook_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button newButton;
+        private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Label firstNameLabel;
+        private System.Windows.Forms.Label lastNameLabel;
+        private System.Windows.Forms.Label phoneNumberLabel;
+        private System.Windows.Forms.Label emailLabel;
+        private System.Windows.Forms.TextBox firstNameTextBox;
+        private System.Windows.Forms.TextBox lastnameTextBox;
+        private System.Windows.Forms.TextBox phoneNumberTextBox;
+        private System.Windows.Forms.TextBox emailTextBox;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Telephone_Book
+{
+    public partial class TelephoneBook : Form
+    {
+ 
+        DataTable dgv = new DataTable() { TableName = "xmlTable" };
+ 
+        bool editing = false;
+
+        private void WriteXmlToFile(DataTable thisDataTable)
+        {
+            if (thisDataTable == null) { return; }
+
+            // Create a file name to write to.
+            string filename = "XmlDoc.xml";
+
+            // Write to the file with the WriteXml method.
+            thisDataTable.WriteXml(filename);
+        }
+
+        public TelephoneBook()
+        {
+            InitializeComponent();
+        }
+
+        private void newButton_Click(object sender, EventArgs e)
+        {
+            firstNameTextBox.Text = "";
+            lastnameTextBox.Text = "";
+            phoneNumberTextBox.Text = "";
+            emailTextBox.Text = "";
+        }
+
+        private void TelephoneBook_Load(object sender, EventArgs e)
+        {
+            dgv.Columns.Add("First Name");
+            dgv.Columns.Add("Last Name");
+            dgv.Columns.Add("Phone number");
+            dgv.Columns.Add("Email");
+            dgv.ReadXml("XmlDoc.xml");
+            dataGridView1.DataSource = dgv;
+        }
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try 
+            {
+                firstNameTextBox.Text = dgv.Rows[dataGridView1.CurrentCell.RowIndex].ItemArray[0].ToString();
+                lastnameTextBox.Text = dgv.Rows[dataGridView1.CurrentCell.RowIndex].ItemArray[1].ToString();
+                phoneNumberTextBox.Text = dgv.Rows[dataGridView1.CurrentCell.RowIndex].ItemArray[2].ToString();
+                emailTextBox.Text = dgv.Rows[dataGridView1.CurrentCell.RowIndex].ItemArray[3].ToString();
+                editing = true;
+            }
+            catch { }
+        }
+        private void loadButton_Click(object sender, EventArgs e)
+        {
+            firstNameTextBox.Text = dgv.Rows[dataGridView1.CurrentCell.RowIndex].ItemArray[0].ToString();
+            lastnameTextBox.Text = dgv.Rows[dataGridView1.CurrentCell.RowIndex].ItemArray[1].ToString();
+            phoneNumberTextBox.Text = dgv.Rows[dataGridView1.CurrentCell.RowIndex].ItemArray[2].ToString();
+            emailTextBox.Text = dgv.Rows[dataGridView1.CurrentCell.RowIndex].ItemArray[3].ToString();
+            editing = true;
+        }
+        private void saveButton_Click(object sender, EventArgs e)
+        {  
+            try
+            {
+                if (editing)
+                {
+                    dgv.Rows[dataGridView1.CurrentCell.RowIndex]["First Name"] = firstNameTextBox.Text;
+                    dgv.Rows[dataGridView1.CurrentCell.RowIndex]["Last Name"] = lastnameTextBox.Text;
+                    dgv.Rows[dataGridView1.CurrentCell.RowIndex]["Phone number"] = phoneNumberTextBox.Text;
+                     dgv.Rows[dataGridView1.CurrentCell.RowIndex]["Email"] = emailTextBox.Text;
+                }
+                else dgv.Rows.Add(firstNameTextBox.Text, lastnameTextBox.Text, phoneNumberTextBox.Text, emailTextBox.Text);
+                editing = false;
+            }
+            catch {}
+            WriteXmlToFile(dgv);
+        }
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                dgv.Rows[dataGridView1.CurrentCell.RowIndex].Delete();
+                WriteXmlToFile(dgv);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Cant delete current entry");
+            }
+        }
+        private void phoneNumberTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsNumber(e.KeyChar) == false)
+            {
+                e.Handled = true;
+            }
+            if (e.KeyChar == (char)Keys.Back)
+            {
+                e.Handled = false;
+            }
+        }
+        private void TelephoneBook_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            WriteXmlToFile(dgv);
+        }
+    }
+}
